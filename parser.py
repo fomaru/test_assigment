adf245c76bc250a46a38e87d3f49bc476b70cfd5
fomaru <you@example.com>
2022-04-28 12:56:09 +0300

    changes

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 import json
 
@@ -24,14 +24,6 @@ def collect_reviews(driver):
         .until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='current-comment']")))
     title = driver.find_element(By.XPATH, "//div[@class='product-name']").text
     for review in reviews:
-        # author_name = WebDriverWait(review, 5, ignored_exceptions=ignored_exceptions) \
-        #     .until(EC.presence_of_element_located((By.XPATH, ".//div[@class='name']"))).text
-        # # grade = len(review.find_elements(By.XPATH, ".//span[@class='full-stars']"))
-        # grade = 5
-        # date = WebDriverWait(review, 5, ignored_exceptions=ignored_exceptions) \
-        #     .until(EC.presence_of_element_located((By.XPATH, ".//div[@class='date']"))).text
-        # text = WebDriverWait(review, 5, ignored_exceptions=ignored_exceptions) \
-        #     .until(EC.presence_of_element_located((By.XPATH, ".//div[@class='comment']"))).text
         try:
             author_name = review.find_element(By.XPATH, ".//div[@class='name']").text
             grade = len(review.find_elements(By.XPATH, ".//span[@class='full-stars']"))
@@ -48,26 +40,39 @@ def collect_reviews(driver):
             }
 
             return_list.append(review_dict)
-            return return_list
-
         except StaleElementReferenceException:
+            print('stale element in collect_reviews, calling func again')
             collect_reviews(driver)
+    return return_list
+
+def collect_urls(driver, xpath):
+
+    urls = []
+    urls_web_elements = []
+
+    elements = driver.find_elements(By.XPATH, xpath)
+    for i in elements:
+        urls_web_elements.append(i)
+    for i in urls_web_elements:
+        urls.append(i.get_attribute("href"))
+
+    return urls
 
 
 def reviews_per_category(driver, page_url):
     driver.get(page_url)
     urls = []
 
-    while check_exists_by_xpath(driver, "//div[@class='back_pagin disabled-arrow']") is False:
-        if check_exists_by_xpath(driver, "//ul[@class='text-n-o-c pages']"):
-            urls_web_elements = driver.find_elements(By.XPATH, "//div[@class='image-place']/a")
-            for i in urls_web_elements:
-                urls.append(i.get_attribute("href"))
+    if check_exists_by_xpath(driver, "//ul[@class='text-n-o-c pages']"):
+        while check_exists_by_xpath(driver, "//div[@class='back_pagin disabled-arrow']") is False:
+            for url in collect_urls(driver, "//div[@class='image-place']/a"):
+                urls.append(url)
             driver.find_element(By.XPATH, "//div[@class='back_pagin']").click()
-            urls_web_elements = driver.find_elements(By.XPATH, "//div[@class='image-place']/a")
-        urls_web_elements = driver.find_elements(By.XPATH, "//div[@class='image-place']/a")
-    for i in urls_web_elements:
-        urls.append(i.get_attribute("href"))
+        for url in collect_urls(driver, "//div[@class='image-place']/a"):
+            urls.append(url)
+    else:
+        for url in collect_urls(driver, "//div[@class='image-place']/a"):
+            urls.append(url)
 
     for url in urls:
         driver.get(url)
@@ -75,12 +80,17 @@ def reviews_per_category(driver, page_url):
         if check_exists_by_xpath(driver, "//div[@class='current-comment']"):
             if check_exists_by_xpath(driver, "//ul[@class='text-n-o-c pages']"):
                 while check_exists_by_xpath(driver, "//div[@class='back_pagin disabled-arrow']") is False:
-                    review_list.append(collect_reviews(driver))
-                    driver.find_element(By.XPATH, "//div[@class='back_pagin']").click()
-                    # driver.implicitly_wait(5)
-                review_list.append(collect_reviews(driver))
+                    for i in collect_reviews(driver):
+                        review_list.append(i)
+                    try:
+                        driver.find_element(By.XPATH, "//div[@class='back_pagin']").click()
+                    except NoSuchElementException:
+                        pass
+                for i in collect_reviews(driver):
+                    review_list.append(i)
             else:
-                review_list.append(collect_reviews(driver))
+                for i in collect_reviews(driver):
+                    review_list.append(i)
 
             with open("reviews/{}.json".format(driver.find_element(By.XPATH, "//div[@class='product-name']").text.replace('/', ' ')), "w") as write_file:
                 json.dump(review_list, write_file, indent=4, ensure_ascii=False)
@@ -90,7 +100,7 @@ def reviews_per_category(driver, page_url):
 
 if __name__ == '__main__':
     options = Options()
-    options.headless = False
+    options.headless = True
     driver = webdriver.Firefox(executable_path='/home/alex/Downloads/geckodriver', options=options)
-    reviews_per_category(driver, "https://eldorado.ua/tablet-pc/c1039006/producer=lenovo/")
+    reviews_per_category(driver, "https://eldorado.ua/smartphones/c1038946/producer=samsung/")
     driver.quit()

b5bf3430645d600a3da05e583a390af0070e8655
fomaru <you@example.com>
2022-04-30 12:16:27 +0300

    added README.md and fixed bugs

---
+++ README.md
@@ -0,0 +1,9 @@
+Don't forget to insert the path to your driver!!!
+
+I was using geckodriver 0.31
+
+Comments on eldorado.ua only contain the date on which they were posted and not the exact time so dates in the resulting file containing all the comments have 00:00:00 as a placeholder for time.
+ 
+The script works fine but is a little bit slow since it's collecting a lot of URLs as an overhead if you wanted to collect more than 3 items per category. Just increase the index value delimiter in reviews_per_sub_category to do so.
+
+Thx, Alexei
--- parser.py
+++ parser.py
@@ -4,12 +4,12 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-
+from datetime import datetime
 import json
 
 ignored_exceptions = (NoSuchElementException, StaleElementReferenceException)
 
-
+# check if element exists using xpath
 def check_exists_by_xpath(driver, xpath):
     try:
         driver.find_element(By.XPATH, xpath)
@@ -17,7 +17,7 @@ def check_exists_by_xpath(driver, xpath):
         return False
     return True
 
-
+# returns list of urls located using xpath
 def collect_urls(driver, xpath):
 
     urls = []
@@ -31,7 +31,7 @@ def collect_urls(driver, xpath):
 
     return urls
 
-
+# collects reviews on the items page
 def collect_reviews(driver):
     return_list = []
     reviews = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions) \
@@ -41,7 +41,8 @@ def collect_reviews(driver):
         try:
             author_name = review.find_element(By.XPATH, ".//div[@class='name']").text
             grade = len(review.find_elements(By.XPATH, ".//span[@class='full-stars']"))
-            date = review.find_element(By.XPATH, ".//div[@class='date']").text
+            date_time_str = "{} 00:00:00".format(review.find_element(By.XPATH, ".//div[@class='date']").text)
+            date = datetime.strptime(date_time_str, '%d.%m.%Y %H:%M:%S').strftime("%Y-%m-%dT%H:%M:%S")
             text = review.find_element(By.XPATH, ".//div[@class='comment']").text
 
             review_dict = {
@@ -54,16 +55,15 @@ def collect_reviews(driver):
             }
             return_list.append(review_dict)
         except StaleElementReferenceException:
-            print("Stale in {}".format(driver.current_url))
             return collect_reviews(driver)
     return return_list
 
-
+# go throught pagination of comments on the items page and call collect_reviews
 def reviews_per_item(driver, page_url):
     driver.get(page_url)
     review_list = []
 
-    if check_exists_by_xpath(driver, "//div[@class='current-comment']"):
+    if check_exists_by_xpath(driver, "//div[@class='tab-item tab-active'][starts-with(.,'Отзывы')]") and check_exists_by_xpath(driver, "//div[@class='current-comment']"):
         if check_exists_by_xpath(driver, "//ul[@class='text-n-o-c pages']"):
             while check_exists_by_xpath(driver, "//div[@class='back_pagin disabled-arrow']") is False:
                 reviews = collect_reviews(driver)
@@ -90,12 +90,12 @@ def reviews_per_item(driver, page_url):
     else:
         print("No comments for {}".format(driver.current_url))
 
-
+# collecting urls to items pages
 def reviews_per_sub_category(driver, page_url):
     driver.get(page_url)
     urls = []
 
-    # go thought pagination pages and collect all urls
+    # handle pagination of the items
     if check_exists_by_xpath(driver, "//ul[@class='text-n-o-c pages']"):
         while check_exists_by_xpath(driver, "//div[@class='back_pagin disabled-arrow']") is False:
             for url in collect_urls(driver, "//div[@class='image-place']/a"):
@@ -106,14 +106,16 @@ def reviews_per_sub_category(driver, page_url):
     else:
         for url in collect_urls(driver, "//div[@class='image-place']/a"):
             urls.append(url)
-
-    for url in urls:
+# how many items per sub-category will be processed
+    for index, url in enumerate(urls):
         reviews_per_item(driver, url)
+        if index == 2:
+            break
 
-
+# opens category page collects urls to subcategories and calls reviews_per_sub_category for all of the urls
 def reviews_per_category(driver, page_url):
     driver.get(page_url)
-    pages = collect_urls(driver, "//div[@class='node-item']/div/a")
+    pages = collect_urls(driver, "(//div[@class='node-item'])/div/a")
     for page in pages:
         reviews_per_sub_category(driver, page)
 
@@ -121,11 +123,11 @@ def reviews_per_category(driver, page_url):
 if __name__ == '__main__':
     options = Options()
     options.headless = True
-    driver = webdriver.Firefox(executable_path='/home/alex/Downloads/geckodriver', options=options)
+    driver = webdriver.Firefox(executable_path='path/to/your/driver', options=options)
     driver.get("https://eldorado.ua/")
     pages = collect_urls(driver, "//div[@class='main-category']//a")
     for index, page in enumerate(pages):
         reviews_per_category(driver, page)
-        if index == 3:
+        if index == 2:
             break
     driver.quit()
